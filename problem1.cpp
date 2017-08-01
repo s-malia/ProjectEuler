ecf2ce3a5d1dc746b3c4b3a088c87059326cdf2d
Malia Smith <maliasmith17@gmail.com>
2017-07-31 18:20:31 -0700

    Add files via upload

---
+++ problem1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+int main() {
+    int sum = 0;
+    for (int i = 1; i < 1000; i++){
+        if (i % 3 == 0 | i % 5 == 0){
+            sum += i;
+        }
+    }
+    cout << sum << endl;
+}
